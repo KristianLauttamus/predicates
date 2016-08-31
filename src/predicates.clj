dfa58c10ad78e8e65d7d4fa14f31d2d049436a29
Kristian Lauttamus <krizzuq@hotmail.com>
2016-09-01 00:56:28 +0300

    Some done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,31 +1,34 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or
+    (= 0 (count string))
+    (= nil string)
+    (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (< 0 (contains? award (:awards book))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
@@ -39,3 +42,28 @@
 (defn prime? [n]
   :-)
 ;^^
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
+
+(has-award? ysabel :world-fantasy) ;=> true
+(has-award? scanner-darkly :hugo)  ;=> false
